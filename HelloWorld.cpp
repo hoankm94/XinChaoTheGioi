287e984664540029b66506a48ca4e57d4e6b607a
Nguyễn Duy Khanh <73708933+hoankm94@users.noreply.github.com>
2022-11-18 16:20:13 +0700

    Testing out conflict

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
+void conflict() {
+    cout << "Trying to create a conflict" << endl;
+}
 int main()
 {
     cout << "Hello World.";
+    conflict;
+    return 0;
 }
