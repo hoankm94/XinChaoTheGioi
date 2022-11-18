c272196d7b98b9e7021a4f85a95befe55e32bd41
Nguyễn Duy Khanh <73708933+hoankm94@users.noreply.github.com>
2022-11-18 15:55:26 +0700

    added conflict function

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+void conflict () {
+    cout << "Trying to create a conflict" << endl;
+}
+
 int main()
 {
     cout << "Hello World.";
     cout << "\nXinChaoTheGioi.";
+    conflict();
+    return 0;
 }
