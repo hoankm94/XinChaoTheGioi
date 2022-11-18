991bc5ce09ef486cbf46d495baf3ed773d2f039f
Nguyễn Duy Khanh <73708933+hoankm94@users.noreply.github.com>
2022-11-18 16:11:16 +0700

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int conflict() {
+    cout << "testing out conflict" << endl;
+    return 69;
+}
 int main()
 {
     cout << "Hello World.";
     cout << "\nXinChaoTheGioi";
+    conflict;
+    return 0;
 }
